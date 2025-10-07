3067dcfad9c759acbcb7393e37139c3f9f4f7f0b
nicolasaraya <nicolasarayaduran@gmail.com>
2025-10-07 01:31:05 -0300

    delete .sh

--- compile.sh
+++
@@ -1,4 +0,0 @@
-compile="g++ *.cpp -O3 -march=native -flto -funroll-loops -fomit-frame-pointer -DNDEBUG -o ng && chmod +x ng"
-
-echo $compile
-eval $compile
--- compiler.sh
+++
@@ -1,1 +0,0 @@
-g++ *.cpp -O3 -march=native -flto -funroll-loops -fomit-frame-pointer -DNDEBUG -o ng && chmod +x ng
